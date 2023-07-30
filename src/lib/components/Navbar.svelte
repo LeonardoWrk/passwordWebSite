368e4ceed99c23f0bf60f7407fe33fc82f4c3063
leonardowrk <leocardozo0704@gmail.com>
2023-07-30 01:22:41 -0300

    começo da pagina principal

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,19 +1,29 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
+	const navs = [
+		{
+			title: 'about',
+			href: '/about'
+		},
+		{
+			title: 'dowload',
+			href: '/dowload'
+		}
+	];
+
 	$: routeId = $page.route.id;
 </script>
 
 <nav>
 	<div class="container">
-		<h1>Projeto</h1>
+		<div class="title">Projeto</div>
 		<ul>
-			<li>
-				<a href="/about" class="hover" class:active={routeId == '/about'}>About</a>
-			</li>
-			<li>
-				<a href="/dowload" class="hover" class:active={routeId == '/dowload'}>Dowload</a>
-			</li>
+			{#each navs as { title, href }}
+				<li>
+					<a class="hover" {href} class:active={routeId == href}>{title}</a>
+				</li>
+			{/each}
 		</ul>
 	</div>
 </nav>
@@ -24,31 +34,47 @@
 		background-color: #5e5b5b;
 	}
 	.container {
+		width: 75%;
+		margin: 0 auto;
+		padding: 20px;
 		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.title {
+		font-size: 1.7em;
+		color: #eddede;
 	}
 	ul {
-		display: flex;
-		margin: 0;
-		margin-left: auto;
+		position: relative;
 		list-style: none;
+		margin: 0;
+		display: flex;
 	}
 	li {
-		margin: 20px;
+		margin: 0 10px;
 		font-size: 1.5em;
-	}
-	h1 {
-		font-size: 1.7em;
-		margin: 20px;
-		color: #eddede;
 	}
 	a {
 		text-decoration: none;
 		color: #eddede;
 	}
 	.active {
-		color: #06062d;
+		color: white;
 	}
 	.hover:hover {
 		color: azure;
+	}
+
+	/* Adiciona uma regra de mídia para dispositivos menores para empilhar os elementos verticalmente */
+	@media (max-width: 768px) {
+		.container {
+			flex-direction: column;
+			align-items: flex-start;
+		}
+
+		.title {
+			margin-bottom: 10px;
+		}
 	}
 </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<div class="container">
+	<div class="text">
+		<h1>
+			tenha sua senha.
+			<strong> mais segura. </strong>
+		</h1>
+	</div>
+</div>
+
+<style>
+	.container {
+		width: 80%;
+		margin: 0 auto;
+		padding: 20px;
+	}
+
+	.text {
+		position: relative;
+		min-height: 1px;
+		padding-right: 15px;
+		padding-left: 15px;
+	}
+	h1 {
+		font-family: 'Segoe UI', 'Helvetica Neue', 'Helvetica', Arial, sans-serif;
+		font-weight: 100;
+		margin-top: 20px;
+		margin-bottom: 10px;
+	}
+	h1 strong {
+		display: block;
+		margin-left: 20px;
+	}
+</style>
